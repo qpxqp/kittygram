d4806bf246a69698d6a27e3053d0a51f0f85918d
Alexander. <dqpxqpb@gmail.com>
2024-05-11 14:52:40 +0300

    Все варианты View реализованы в проекте

--- cats/views.py
+++ cats/views.py
@@ -1,11 +1,15 @@
+from rest_framework import generics
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
+# =======================================================================
+# НА ФУНКЦИЯХ
 @api_view(['GET', 'POST'])
 def cat_list(request):
     if request.method == 'POST':
@@ -17,3 +21,75 @@ def cat_list(request):
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def cat_list_detail(request, cat_id):
+    cat = Cat.objects.get(pk=cat_id)
+    if request.method == 'PUT' or request.method == 'PATCH':
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        cat.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    serializer = CatSerializer(cat)
+    return Response(serializer.data)
+
+
+# =======================================================================
+# НА КЛАССАХ (низкоуровневые)
+class APICat(APIView):
+    def get(self, request):
+        cats = Cat.objects.all()
+        serializer = CatSerializer(cats, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CatSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class APICatDetail(APIView):
+    def get(self, request, cat_id):
+        cat = Cat.objects.get(pk=cat_id)
+        serializer = CatSerializer(cat)
+        return Response(serializer.data)
+
+    def put(self, request, cat_id):
+        cat = Cat.objects.get(pk=cat_id)
+        serializer = CatSerializer(cat, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, cat_id):
+        cat = Cat.objects.get(pk=cat_id)
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, cat_id):
+        cat = Cat.objects.get(pk=cat_id)
+        cat.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# =======================================================================
+# НА КЛАССАХ (высокоуровневые, Generic)
+class CatList(generics.ListCreateAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
 
-from cats.views import cat_list
+from cats.views import cat_list, cat_list_detail  # НА ФУНКЦИЯХ
+from cats.views import APICat, APICatDetail       # НА КЛАССАХ (низкоуровневые)
+from cats.views import CatList, CatDetail         # НА КЛАССАХ (высокоуровневые, Generic)
+
 
 urlpatterns = [
-   path('cats/', cat_list),
+    # НА ФУНКЦИЯХ
+    # path('api/v1/cats/', cat_list),
+    # path('api/v1/cats/<int:cat_id>/', cat_list_detail),
+
+    # НА КЛАССАХ (низкоуровневые)
+    # path('api/v1/cats/', APICat.as_view()),
+    # path('api/v1/cats/<int:cat_id>/', APICatDetail.as_view()),
+
+    # НА КЛАССАХ (высокоуровневые, Generic)
+    path('api/v1/cats/', CatList.as_view()),
+    path('api/v1/cats/<int:pk>/', CatDetail.as_view()),
 ]
-
-
